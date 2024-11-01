366aea245430ba199da509d0a98764190c1b168f
vstupitsyn <vstupitsyn@ipg.ru>
2024-11-01 15:28:19 +0300

    layout register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  form: FormGroup;
+
+  private fb = inject(FormBuilder)
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void{
+    this.form = this.fb.group({
+      username: '',
+      email: '',
+      password: ''
+    })
+    console.log(this.form)
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value)
+  }
 
 }
