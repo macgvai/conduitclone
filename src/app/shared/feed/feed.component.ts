5e79c00c23d323c98ca7997b1e8838f80f512a11
vstupitsyn <vstupitsyn@ipg.ru>
2024-11-18 10:36:43 +0300

    4.32

--- src/app/shared/feed/feed.component.ts
+++ src/app/shared/feed/feed.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import {Component, inject, Input, OnDestroy, OnInit} from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { getFeedAction } from './store/actions/getFeed.action';
-import { map, Observable } from 'rxjs';
+import {map, Observable, Subscription} from 'rxjs';
 import { GetFeedResponseInterface } from './types/getFeedResponse.interface';
 import {
     errorSelector,
@@ -9,10 +9,11 @@ import {
     isLoadingSelector,
 } from './store/selectors';
 import { AsyncPipe, NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import {ActivatedRoute, Params, Router, RouterLink} from '@angular/router';
 import { BannerComponent } from '../banner/banner.component';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { LoadingComponent } from '../loading/loading.component';
+import {environment} from '../../../environments/environment.development';
 
 @Component({
     selector: 'mc-feed',
@@ -30,30 +31,46 @@ import { LoadingComponent } from '../loading/loading.component';
     templateUrl: './feed.component.html',
     styleUrl: './feed.component.scss',
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
     @Input('apiUrl') apiUrlProps: string;
 
     store = inject(Store);
+    router = inject(Router);
+    route = inject(ActivatedRoute);
 
     feed$: Observable<GetFeedResponseInterface | null>;
     isLoading$: Observable<boolean>;
     error$: Observable<string | null>;
+    limit: number = environment.limit;
+    baseUrl: string;
+    currentPage: number;
+    queryParamsSubscription: Subscription;
 
     ngOnInit(): void {
         this.initializeValues();
         this.fetchData();
-        this.feed$.pipe(map((el) => el?.articles)).subscribe((state) => {
-            console.log(state);
-        });
+        this.initializeListeners();
     }
 
     initializeValues(): void {
         this.feed$ = this.store.pipe(select(feedSelector));
         this.isLoading$ = this.store.pipe(select(isLoadingSelector));
         this.error$ = this.store.pipe(select(errorSelector));
+        this.baseUrl = this.router.url.split('?')[0]
+    }
+
+    initializeListeners(): void {
+        this.queryParamsSubscription = this.route.queryParams.subscribe((params: Params) => {
+                this.currentPage = Number(params['page'] || '1');
+            }
+        )
     }
 
     fetchData(): void {
         this.store.dispatch(getFeedAction({ url: this.apiUrlProps }));
+    }
+
+    ngOnDestroy(): void {
+        this.queryParamsSubscription.unsubscribe();
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://194.87.147.141:3000/api'
+  apiUrl: 'http://194.87.147.141:3000/api',
+    limit: 10,
+
 
 };
