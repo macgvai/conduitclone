31eec1a1dab72253b288ba18227f1f3c5d9f7f01
vstupitsyn <vstupitsyn@ipg.ru>
2024-12-04 17:15:57 +0300

    6.40

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,34 +1,79 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {select, Store} from '@ngrx/store';
 import {getArticleAction} from './store/actions/getArticle.action';
 import {ActivatedRoute} from '@angular/router';
-import {getPopularTagsAction} from '../shared/popular-tags/store/actions/getTags.action';
+import {ArticleInterface} from '../shared/types/article.interface';
+import {combineLatest, combineLatestWith, elementAt, map, Observable, Subscription} from 'rxjs';
+import {errorSelector, getArticleSelector, isLoadingArticleSelector} from './store/selectors';
+import {currentUserSelector} from '../store/selectors';
+import {CurrentUserInterface} from '../shared/types/currentUser.interface';
+import {AsyncPipe} from '@angular/common';
+import {log} from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
 
 @Component({
   selector: 'mc-article',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, OnDestroy {
   store = inject(Store)
   route = inject(ActivatedRoute)
 
-  urlSlug: string
+  slug: string
+  article: ArticleInterface | null;
+  articleSubscription: Subscription;
+  isLoading$: Observable<boolean>;
+  error$: Observable<string | null>;
+  isAuthor$: Observable<boolean>;
 
   ngOnInit(): void {
     this.initializeValues();
+    this.initializeListeners();
     this.fetchData();
   }
 
   initializeValues() {
-    this.route.params.subscribe(params => {
-      this.urlSlug = params['slug']
-    })
+    this.slug = this.route.snapshot.paramMap.get('slug');
+    this.isLoading$ = this.store.pipe(select(isLoadingArticleSelector));
+    this.error$ = this.store.pipe(select(errorSelector));
+  }
+
+  initializeListeners() {
+    this.articleSubscription = this.store
+      .pipe(select(getArticleSelector))
+      .subscribe((article: ArticleInterface | null) => {
+        this.article = article
+      })
+
+    // deprecated
+    // this.isAuthor$ = combineLatest(
+    //   this.store.pipe(select(getArticleSelector)),
+    //   this.store.pipe(select(currentUserSelector))
+    // ).pipe(map(([article, currentUser]: [ArticleInterface | null, CurrentUserInterface | null]) => {
+    //   console.log(article, currentUser);
+    //   return false;
+    // }))
+
+    this.isAuthor$ = this.store.pipe(select(getArticleSelector)).pipe(
+      combineLatestWith(this.store.pipe(select(currentUserSelector))),
+      map(([article, currentUser]: [ArticleInterface | null, CurrentUserInterface | null]) => {
+
+        console.log(article, currentUser);
+
+        return false
+      })
+    )
   }
 
   fetchData() {
-    this.store.dispatch(getArticleAction({slug: this.urlSlug}))
+    this.store.dispatch(getArticleAction({slug: this.slug}))
+  }
+
+  ngOnDestroy(): void {
+    this.articleSubscription.unsubscribe()
   }
 }
